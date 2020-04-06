ddf0cff76f712c5578aff395c88452b51a19788f
Administrator <Administrator@BATAR02M002.rete.poste>
2020-04-06 18:37:56 +0200

    copiato il codice dell'esercizio anagramma by fulcorno

--- Lab05_Anagrammi/src/main/java/it/polito/tdp/anagrammi/model/TestRicerca.java
+++ Lab05_Anagrammi/src/main/java/it/polito/tdp/anagrammi/model/TestRicerca.java
@@ -1,10 +1,18 @@
 package it.polito.tdp.anagrammi.model;
 
+import java.util.List;
+
+
+
 public class TestRicerca {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+Ricerca ric = new Ricerca() ;
+		
+		List<String> ana_dog = ric.anagrammi("dog") ;
+		System.out.println(ana_dog) ;
 	}
 
 }
