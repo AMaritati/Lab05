29637d17c616b7ba475cbfe4ccbe64c79585825e
Administrator <Administrator@BATAR02M002.rete.poste>
2020-04-06 18:23:16 +0200

    Creazione classi con collegamento al DB

---
+++ Lab05_Anagrammi/src/main/java/it/polito/tdp/anagrammi/DAO/TestDB.java
@@ -0,0 +1,13 @@
+package it.polito.tdp.anagrammi.DAO;
+
+public class TestDB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		AnagrammaDAO aDAO = new AnagrammaDAO();
+		System.out.println();
+
+	}
+
+}
---
+++ Lab05_Anagrammi/src/main/java/it/polito/tdp/anagrammi/model/TestRicerca.java
@@ -0,0 +1,10 @@
+package it.polito.tdp.anagrammi.model;
+
+public class TestRicerca {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
