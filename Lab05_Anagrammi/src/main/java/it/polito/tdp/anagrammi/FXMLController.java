37c7d12aeb042f6d98d8774f651e1dad28b91f39
Administrator <Administrator@BATAR02M002.rete.poste>
2020-04-06 18:00:17 +0200

    SCENA GRAFICA COMPLETATA

--- Lab05_Anagrammi/src/main/java/it/polito/tdp/anagrammi/FXMLController.java
+++ Lab05_Anagrammi/src/main/java/it/polito/tdp/anagrammi/FXMLController.java
@@ -1,6 +1,53 @@
 package it.polito.tdp.anagrammi;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
 
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField txtParola;
+
+    @FXML
+    private Button btnCalcolaAnagramma;
+
+    @FXML
+    private TextArea txtOK;
+
+    @FXML
+    private TextArea txtNOT;
+
+    @FXML
+    private Button btnReset;
+
+    @FXML
+    void doAnagramma(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doReset(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnCalcolaAnagramma != null : "fx:id=\"btnCalcolaAnagramma\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtOK != null : "fx:id=\"txtOK\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtNOT != null : "fx:id=\"txtNOT\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
+
+    }
 }
